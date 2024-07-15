a9c14ace62eab94efebc1b9af6ce82e16b58a690
David <59463977+TaintedIron@users.noreply.github.com>
2024-07-15 18:41:21 +0000

    CodeChallenge #2

--- CodeChallenge1.sql
+++ CodeChallenge1.sql
@@ -1,6 +1,11 @@
-SELECT team, shirt_or_hat, first_name, last_name
+SELECT team,
+  shirt_or_hat,
+  first_name,
+  last_name
 FROM people
-LEFT OUTER JOIN states ON people.state_code = states.state_abbrev
+  LEFT OUTER JOIN states ON people.state_code = states.state_abbrev
 WHERE people.state_code = 'CO'
-ORDER BY team, shirt_or_hat, last_name DESC
+ORDER BY team,
+  shirt_or_hat,
+  last_name DESC
 LIMIT 11;
---
+++ CodeChallenge2.sql
@@ -0,0 +1,8 @@
+SELECT states.region,
+  people.team,
+  COUNT(people.team)
+FROM states
+  JOIN people ON states.state_abbrev = people.state_code
+GROUP BY states.region,
+  people.team
+LIMIT 5;
